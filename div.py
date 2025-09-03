0f6dffa12b34772edb9d6cfe18928bfd296d48ab
Delietan <martinusnord@gmail.com>
2025-09-03 13:56:10 +0200

    testin

--- div.py
+++ div.py
@@ -1,1 +1,15 @@
 print("Kake")
+
+tall = 10
+
+# tall = int(input("Skriv et fall: "))
+print("Ditt tall er: ", tall)
+
+pant = 6
+pant = pant + 7
+
+print(pant)
+
+saft = "kakepanne"
+
+print("lengden er: ",len(saft))
