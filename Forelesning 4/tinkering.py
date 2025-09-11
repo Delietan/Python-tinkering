4ff5389fee10ccace3c7c33edefbbabd5d0d8082
Delietan <martinusnord@gmail.com>
2025-09-11 14:02:51 +0200

    fremgang

---
+++ Forelesning 4/tinkering.py
@@ -0,0 +1,24 @@
+navneliste = [1, 2, 3]
+navneliste.append("Laks")
+
+# for navn in navneliste:
+#     print(navn)
+
+# for i in range(4):
+#     print(navneliste[i])
+
+# x = 1
+
+# while x < 100:
+#     print(x)
+#     x += 2
+
+# x = 0
+
+# while x < 100:
+#     if x % 2 == 1:
+#         print(x)
+#     x += 1
+
+for x in range(1,100,2):
+    print(x)
